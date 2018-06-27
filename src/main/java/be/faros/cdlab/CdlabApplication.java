30a40e9cfa0f71cff396c9cb9b39eb997098da1b
Sven Smets <Sven.Smet@external.toyota-europe.com>
2018-06-27 15:54:25 +0200

    initial commit

--- src/main/java/be/faros/cdlab/CdlabApplication.java
+++ src/main/java/be/faros/cdlab/CdlabApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CdlabApplication {
 
 	public static void main(String[] args) {
+
+
 		SpringApplication.run(CdlabApplication.class, args);
 	}
 }
