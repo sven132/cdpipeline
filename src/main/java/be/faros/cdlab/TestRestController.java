682758e56b79fb86ccc215f0cd6181517f2d28dc
Sven Smets <Sven.Smet@external.toyota-europe.com>
2018-06-27 16:08:29 +0200

    first test

---
+++ src/main/java/be/faros/cdlab/TestRestController.java
@@ -0,0 +1,14 @@
+package be.faros.cdlab;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestRestController {
+
+    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
+    public String getCategories() {
+        return "Hello";
+    }
+}
---
+++ src/test/java/be/faros/cdlab/Test.java
@@ -0,0 +1,12 @@
+package be.faros.cdlab;
+
+import org.junit.Assert;
+
+
+public class Test {
+
+    @org.junit.Test
+    public void test(){
+        Assert.assertTrue(true);
+    }
+}
